ac77b57a202885f69c135d90c94d0a9f6aa3a011
pbaranchuk <pobaranchukv@gmail.com>
2023-09-11 19:54:42 +0200

    Next commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import './App.css';
+import './ToDoList';
+import ToDoList from "./ToDoList";
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <ToDoList title={"What to learn"}/>
+            <ToDoList title={"What to buy"}/>
         </div>
     );
 }
---
+++ src/ToDoList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type ToDoListPropsType = {
+    title: string;
+}
+const ToDoList: React.FC<ToDoListPropsType> = (props) => {
+    return (
+        <div className="todolist">
+            <div>
+                <h3>{props.title}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ToDoList;
