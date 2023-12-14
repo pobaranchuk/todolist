2fd8d8ce988ac32439b8fa39e99d6a3612a43715
VPobaranchuk <pobaranchukv@gmail.com>
2023-12-14 13:22:31 +0100

    New project from incubator

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AppWithRedux from "./AppWithRedax";
-
-test('renders learn react link', () => {
-  const { getByText } = render(<AppWithRedux />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
