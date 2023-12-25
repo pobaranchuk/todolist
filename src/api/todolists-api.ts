d179a453f0c3d8954b9fdda5a0234e34ddc531bb
VPobaranchuk <pobaranchukv@gmail.com>
2023-12-25 14:27:47 +0100

    Lesson 15 done

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,10 +1,10 @@
-import axios, {AxiosResponse} from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': '5fc11a34-7258-4926-8c00-915edb4f940c'
     }
 })
 
@@ -14,9 +14,7 @@ export const todolistsAPI = {
         return instance.get<TodolistType[]>('todo-lists');
     },
     createTodolist(title: string) {
-        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{
-            item: TodolistType
-        }>>, { title: string }>('todo-lists', {title});
+        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>,{ title: string }>('todo-lists', {title});
     },
     deleteTodolist(id: string) {
         return instance.delete<ResponseType>(`todo-lists/${id}`);
@@ -31,14 +29,10 @@ export const todolistsAPI = {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
     },
     createTask(todolistId: string, title: string) {
-        return instance.post<ResponseType<{ item: TaskType }>, AxiosResponse<ResponseType<{ item: TaskType }>>, {
-            title: string
-        }>(`todo-lists/${todolistId}/tasks`, {title});
+        return instance.post<ResponseType<{ item: TaskType }>, AxiosResponse<ResponseType<{ item: TaskType }>>, { title: string }>(`todo-lists/${todolistId}/tasks`, {title});
     },
     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<ResponseType<{ item: TaskType }>, AxiosResponse<ResponseType<{
-            item: TaskType
-        }>>, UpdateTaskModelType>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
+        return instance.put<ResponseType<{ item: TaskType }>, AxiosResponse<ResponseType<{ item: TaskType }>>, UpdateTaskModelType>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
     }
 }
 
---
+++ src/app/app-reducer.ts
@@ -0,0 +1,36 @@
+//app-reducer.tsx
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+    status: 'loading' as RequestStatusType,
+    error: null as null | string
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR': {
+            return {...state, error: action.error}
+        }
+        default:
+            return state
+    }
+}
+
+export const setAppStatusAC = (status: RequestStatusType) => {
+    return {
+        type: 'APP/SET-STATUS',
+        status
+    } as const
+}
+
+export const setAppErrorAC = (error: null | string) => {
+    return {
+        type: 'APP/SET-ERROR', error
+    } as const
+}
+
+export type ActionsType = ReturnType<typeof setAppStatusAC> | ReturnType<typeof setAppErrorAC>
