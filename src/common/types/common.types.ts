c71f4f9f3f87b23dc792bc6ed11f371b0ed07128
VPobaranchuk <pobaranchukv@gmail.com>
2024-02-08 13:11:11 +0100

    Lesson 20

--- src/common/types/common.types.ts
+++ src/common/types/common.types.ts
@@ -1,5 +1,11 @@
-export type ResponseType<D = {}> = {
+type FieldErrorType = {
+  error: string;
+  field: string;
+};
+
+export type BaseResponseType<D = {}> = {
   resultCode: number;
   messages: Array<string>;
   data: D;
+  fieldsErrors: FieldErrorType[];
 };
--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -1,12 +1,13 @@
 import { Dispatch } from "redux";
 import { appActions } from "app/app.reducer";
-import { ResponseType } from "common/types/common.types";
+import { BaseResponseType } from "../types";
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
-  if (data.messages.length) {
-    dispatch(appActions.setAppError({ error: data.messages[0] }));
-  } else {
-    dispatch(appActions.setAppError({ error: "Some error occurred" }));
+export const handleServerAppError = <D>(
+  data: BaseResponseType<D>,
+  dispatch: Dispatch,
+  showError: boolean = true,
+): void => {
+  if (showError) {
+    dispatch(appActions.setAppError({ error: data.messages.length ? data.messages[0] : "Some error occurred" }));
   }
-  dispatch(appActions.setAppStatus({ status: "failed" }));
 };
